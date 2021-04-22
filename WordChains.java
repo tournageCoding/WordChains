4361e42d27aa396eb2918f95419acda93e4427f7
tournageCoding <jakobharvey1212@gmail.com>
2021-04-22 21:54:20 +1200

    Added WordChains.java with constructor

---
+++ WordChains.java
@@ -0,0 +1,10 @@
+public class WordChains {
+
+    public WordChains(String start, String end) {
+        this.shortestPath(start, end);
+    }
+
+    private void shortestPath(String start, String end) {
+        System.out.println(start + " " + end);
+    }
+}
